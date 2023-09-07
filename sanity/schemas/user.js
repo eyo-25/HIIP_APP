78b9fee23be8e2cfd9bc5ec8720914e1e3b9b1b9
eyo25 <qutuzm@naver.com>
2023-09-07 19:36:08 +0900

    feat: user 스키마 정의

---
+++ sanity/schemas/user.js
@@ -0,0 +1,33 @@
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    {
+      title: 'Username',
+      name: 'username',
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Image',
+      name: 'image',
+      type: 'string',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'username',
+    },
+  },
+}
